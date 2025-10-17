b7b3114794543277b6ba15a3005a0d9dbe045095
Huy <huytran19052005@gmail.com>
2025-10-17 14:32:40 +0100

    Initial commit

---
+++ src/CaesarCipherApp/Main.java
@@ -0,0 +1,15 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CaesarCipherApp;
+
+/**
+ *
+ * @author H P
+ */
+public class Main {
+    public static void main(String[] args){//khởi tạo hàm main để chạy chương trình
+        new MainFrame().setVisible(true);//tạo một cửa sổ giao diện mới và hiển thị nó lên màn hình.
+    }
+}
